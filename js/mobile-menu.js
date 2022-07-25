691ed89cea0661531cf1073c889d6b1af70a9a43
Max-Morozynskyi <m0r0zin@ukr.net>
2022-07-26 01:11:27 +0300

    footer done

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,4 +1,3 @@
-
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
@@ -11,5 +10,12 @@
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
+  });
+   // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenuRef.classList.remove('is-open');
+    menuBtnRef.classList.remove('is-open');
+    menuBtnRef.setAttribute('aria-expanded', false);
   });
 })();

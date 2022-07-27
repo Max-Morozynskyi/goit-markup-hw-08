ebce2215788b0619c8d0eade3f8327bf18744870
Max-Morozynskyi <m0r0zin@ukr.net>
2022-07-27 23:12:29 +0300

    modals scroll fixed

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,15 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const bodyRef = document.querySelector("[data-modal-scroll]");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
-
+    bodyRef.classList.toggle("no-scroll");
     mobileMenuRef.classList.toggle("is-open");
   });
    // Close the mobile menu on wider screens if the device orientation changes
@@ -16,6 +17,7 @@
     if (!e.matches) return;
     mobileMenuRef.classList.remove('is-open');
     menuBtnRef.classList.remove('is-open');
+    bodyRef.classList.remove("no-scroll");
     menuBtnRef.setAttribute('aria-expanded', false);
   });
 })();
